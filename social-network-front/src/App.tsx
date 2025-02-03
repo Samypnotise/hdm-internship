f6bfccf3e1a87c1bf3aa4f466561221881a80fb2
Samypnotise <SamyDjemiliPRO@gmail.com>
2025-02-03 14:14:32 +0100

    chore: refactoring project using shadcn

--- social-network-front/src/App.tsx
+++ social-network-front/src/App.tsx
@@ -1,33 +1,3 @@
-import React from 'react';
-
-import {
-  ApolloClient,
-  ApolloProvider,
-  HttpLink,
-  InMemoryCache,
-} from '@apollo/client';
-
-import { BrowserRouter, Route, Routes } from 'react-router';
-
-import './assets/global.css';
-import { LoginView } from './view';
-
-const cache = new InMemoryCache({});
-const link = new HttpLink();
-
-const apolloClient = new ApolloClient({
-  cache,
-  link,
-});
-
-const App = () => (
-  <ApolloProvider client={apolloClient}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="login" element={<LoginView />} />
-      </Routes>
-    </BrowserRouter>
-  </ApolloProvider>
-);
+const App = () => <>Put content here</>;
 
 export default App;
--- social-network-front/src/view/LoginView.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const LoginView = () => {
-  return <div>toto2</div>;
-};
-
-export default LoginView;
