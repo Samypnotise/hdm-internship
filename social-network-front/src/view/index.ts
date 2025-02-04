0e267a55a8e2171f20219dbaabe49d2f4c0c7025
Samypnotise <SamyDjemiliPRO@gmail.com>
2025-02-04 16:30:34 +0100

    feat(HomeView): adding HomeView dummy component

--- social-network-front/src/view/index.ts
+++ social-network-front/src/view/index.ts
@@ -1,2 +1,3 @@
 export { default as LoginView } from './LoginView/LoginView';
 export { default as RegisterView } from './RegisterView/RegisterView';
+export { default as HomeView } from './HomeView';
